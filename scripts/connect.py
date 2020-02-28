bb6291455f44fe7a17b73d66c4c5d1746662ef38
danbenner <12009658+danbenner@users.noreply.github.com>
2020-02-28 13:01:43 -0600

    Update connect.py

--- scripts/connect.py
+++ scripts/connect.py
@@ -2,9 +2,9 @@ import teradata
 import os
 
 # -------------------- INIT -------------------- #
-EDW_DNS_NAME = os.environ["EDW_DNS_NAME"] # "EDWT"
-EDW_USERNAME = os.environ["EDW_USERNAME"] # ServiceAccountID
-EDW_PASSWORD = os.environ["EDW_PASSWORD"] # ServiceAccountPass
+DNS_NAME = os.environ["DNS_NAME"]
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
 GOPATH = os.environ["GOPATH"]
 OUTPUT_FILE_NAME = os.path.abspath("/data.json")
 
@@ -14,8 +14,8 @@ if os.path.exists(OUTPUT_FILE_NAME):
 
 # ----------- SETUP TERADATA SESSION ----------- #
 udaExec = teradata.UdaExec (appName="test", version="1.0", logConsole=False)
-session = udaExec.connect(method="odbc", system=EDW_DNS_NAME,
-        username=EDW_USERNAME, password=EDW_PASSWORD)
+session = udaExec.connect(method="odbc", system=DNS_NAME,
+        username=USERNAME, password=PASSWORD)
 
 # --------------- READ SQL QUERY --------------- #
 path = os.path.abspath(GOPATH + "/src/scriptRunner/sql/test.sql")
